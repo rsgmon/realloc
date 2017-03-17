b98efefdaba2afd53e2379b4db6d07348ffe6592
Ryeland Gongora <rsgmon@gmail.com>
2017-03-17 08:49:07 -0700

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='portmgr',
+    version='0.1',
+    description='A portfolio management api',
+    url='http://github.com/rsgmon/portmgr',
+    author='Ryeland Gongora',
+    author_email='rsgmon@gmail.com',
+    packages= ['TradeManager'],
+    license='MIT',
+    zip_safe=False)
