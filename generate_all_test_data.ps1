2868b5883af5d35c8f385270be473bcd69df9de4
Ryeland Gongora <rsgmon@gmail.com>
2017-08-10 13:01:40 -0700

    added all test data and scripts

---
+++ generate_all_test_data.ps1
@@ -0,0 +1,6 @@
+python .\TradeManager\test\test_data_generator.py buysOnly BuyOnlyRequest.xlsx buysOnly
+python .\TradeManager\test\test_data_generator.py buysOnly\singleAccount TradeRequest.xlsx buysOnly\singleAccount
+python .\TradeManager\test\test_data_generator.py sellsOnly TradeRequest.xlsx sellsOnly
+python .\TradeManager\test\test_data_generator.py sellsOnly\sellsOnlySingle SellOnlySingleAccount.xlsx sellsOnly\sellsOnlySingle
+python .\TradeManager\test\test_data_generator.py sellsAndBuys\singleAccount TradeRequest.xlsx sellsAndBuys\singleAccount
+
