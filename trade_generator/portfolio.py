926dd6f1b97a07eff58dbdb064dced9fe2f3a3a6
Ryeland <rsgmon@gmail.com>
2025-02-18 16:52:37 -0800

    Refactor based on chatgpt.

--- trade_generator/portfolio.py
+++ trade_generator/portfolio.py
@@ -1,132 +1,97 @@
 import pandas as pd
 import numpy as np
 
-class Portfolio(object):
-    def __init__(self, portfolio_request, prices, *args):
+class Portfolio:
+    def __init__(self, portfolio_request, prices):
         """
-        Returns a portfolio object
-        :param portfolio_request: dataframe containing accounts, positions, and restrictions.
-        :param prices: dataframe containing symbols and prices
-        :param args: only for testing, can be any string and prevents some init
+        Initializes a Portfolio object.
+        :param portfolio_request: DataFrame containing accounts, positions, and restrictions.
+        :param prices: DataFrame containing symbols and prices.
         """
         self.portfolio_request = portfolio_request
         self.prices = prices
         self.account_numbers = self.portfolio_request['account_number'].unique()
-        if len(args) == 0:
-            self._assemble_accounts()
-            self.set_portfolio_positions_and_value()
-            self._account_position_matrix = self.create_account_matrix()
-            self.create_cash_matrix()
+        self.accounts = self._assemble_accounts()
+        self.set_portfolio_positions_and_value()
+        self._account_matrix = self.create_account_matrix()
+        self.create_cash_matrix()
 
-
-    def _assemble_accounts(self, *args):
-        """
-            sets self.accounts to list of account objects.
-        """
-        self.accounts = []
-
-        if len(args) == 0:
-            for account in self.account_numbers:
-                self.accounts.append(Account(self.portfolio_request[self.portfolio_request.loc[:, 'account_number'] == account]))
-        else:
-            for account in self.account_numbers:
-                self.accounts.append(Account(self.portfolio_request[self.portfolio_request.loc[:, 'account_number'] == account], args))
+    def _assemble_accounts(self):
+        """ Creates a list of Account objects from portfolio request. """
+        return [Account(self.portfolio_request[self.portfolio_request['account_number'] == acc]) for acc in self.account_numbers]
 
     def _aggregate_share_positions(self):
-        """
-        Creates a single dataframe of account positions grouped by symbol.
-        :return: dataframe
-        """
-        portfolio_concatenated_positions = pd.DataFrame()
-        for account in self.accounts:
-            portfolio_concatenated_positions = pd.concat([portfolio_concatenated_positions, account.account_positions])
-        return portfolio_concatenated_positions.groupby(level=0).sum()
+        """ Aggregates share positions across all accounts. """
+        return pd.concat([account.account_positions for account in self.accounts]).groupby(level=0).sum()
 
     def set_portfolio_cash(self):
-        self.portfolio_cash = 0
-        for account in self.accounts:
-            self.portfolio_cash += account.account_cash.loc[account.account_number[0],  'shares']
-
+        """ Calculates total portfolio cash. """
+        self.portfolio_cash = sum(account.account_cash.get(account.account_number[0], 0) for account in self.accounts)
 
     def set_portfolio_positions_and_value(self):
-        """
-        Sets attribute portfolio_positions to have aggregated positions
-        :return: none
-        """
+        """ Sets aggregated portfolio positions and calculates total value. """
         self.portfolio_positions = self._aggregate_share_positions()
         self.set_portfolio_cash()
-        if len(self.portfolio_positions) > 0:
-            self.portfolio_positions['price'] = self.prices
-            self.portfolio_positions['position'] = self.portfolio_positions['shares']* self.portfolio_positions['price']
+
+        if not self.portfolio_positions.empty:
+            self.portfolio_positions['price'] = self.prices.reindex(self.portfolio_positions.index, fill_value=0)
+            self.portfolio_positions['position'] = self.portfolio_positions['shares'] * self.portfolio_positions['price']
             self.portfolio_value = round(self.portfolio_positions['position'].sum() + self.portfolio_cash, 2)
-            self.portfolio_positions['portfolio_weight'] = self.portfolio_positions['position']/self.portfolio_value
+            self.portfolio_positions['portfolio_weight'] = self.portfolio_positions['position'] / max(self.portfolio_value, 1)
         else:
-            self.portfolio_positions= pd.DataFrame(columns=['shares', 'price','position', 'portfolio_weight'])
+            self.portfolio_positions = pd.DataFrame(columns=['shares', 'price', 'position', 'portfolio_weight'])
             self.portfolio_value = self.portfolio_cash
 
     def create_cash_matrix(self):
-        self._cash_matrix = pd.DataFrame()
-        for account in self.accounts:
-            self._cash_matrix = pd.concat([self._cash_matrix, account.account_cash])
+        """ Creates a DataFrame tracking cash balances across accounts. """
+        self._cash_matrix = pd.concat([account.account_cash for account in self.accounts])
 
     def create_account_matrix(self):
-        account_matrix = pd.DataFrame()
-        for account in self.accounts:
-            account_matrix = pd.concat([account_matrix, account.account_positions])
-        return account_matrix
+        """ Creates a DataFrame tracking positions across accounts. """
+        return pd.concat([account.account_positions for account in self.accounts])
 
     @property
     def account_matrix(self):
-        return self._account_position_matrix
+        return self._account_matrix
 
     @property
     def cash_matrix(self):
         return self._cash_matrix
 
     def __str__(self):
-            return '\n\n'.join(['{key}\n{value}'.format(key=key, value=self.__dict__.get(key)) for key in self.__dict__])
-
-class PostTradePortfolio(Portfolio):
-    def __init__(self, trade_instructions, portfolio_request, prices):
-        Portfolio.__init__(self, portfolio_request, prices)
-        self.update_accounts(trade_instructions)
-
-    def update_accounts(self, trade_instructions):
-        for number in self.account_numbers:
-            cash_change = trade_instructions.copy()
-            cash_change['cash_change'] = (cash_change['shares'] * self.prices.price)
-            account_trade_instructions = cash_change.groupby('account').get_group(number)
-            for account in self.accounts:
-                if account.account_number[0] == number:
-                    account.update_account(account_trade_instructions)
-        self.set_portfolio_positions_and_value()
-        self._account_position_matrix = self.create_account_matrix()
-        self.create_cash_matrix()
+        return '\n\n'.join([f'{key}\n{value}' for key, value in self.__dict__.items()])
 
 
-class Account(object):
-    def __init__(self, request, *args):
+class Account:
+    def __init__(self, request):
+        """ Initializes an Account object from a request DataFrame. """
         self.account_raw = request
-        self.account_number = self.account_raw['account_number'].unique()
-        if len(args) == 0:
-            self.account_positions = self.account_raw.loc[self.account_raw.loc[:, 'symbol'] != 'account_cash'].dropna(subset=['symbol']).drop(['restrictions'], 1).set_index(['symbol', 'account_number'])
-            self.account_cash = self.account_raw.loc[self.account_raw.loc[:, 'symbol'] == 'account_cash'].dropna(subset=['symbol']).drop(['restrictions', 'symbol'], 1).set_index(['account_number'])
-            self.account_level_restrictions = self.account_raw.dropna(subset=['restrictions']).drop(['symbol', 'shares'], 1)
-
-        # todo self.position_level_restrcitions
+        self.account_number = self.account_raw['account_number'].unique()[0]
+        self.account_positions = (
+            self.account_raw[self.account_raw['symbol'] != 'account_cash']
+                .dropna(subset=['symbol'])
+                .set_index(['symbol', 'account_number'])
+        )
+        self.account_cash = (
+            self.account_raw[self.account_raw['symbol'] == 'account_cash']
+                .dropna(subset=['symbol'])
+                .set_index(['account_number'])
+        )
 
     def update_account(self, trades):
+        """ Updates account positions and cash based on executed trades. """
         self.update_cash(trades)
         self.update_positions(trades)
 
     def update_cash(self, trades):
-        self.account_cash.set_value('account_cash', 'shares', self.account_cash.loc['account_cash', 'shares'] - trades['cash_change'].sum())
+        """ Updates cash balance after trades. """
+        cash_change = trades.groupby('account_number')['cash_change'].sum()
+        self.account_cash.loc[:, 'shares'] -= cash_change.get(self.account_number, 0)
 
     def update_positions(self, trades):
-        self.account_positions = pd.concat([self.account_positions, pd.DataFrame(trades['shares'])]).groupby(level=0).sum()
-
-
-
+        """ Updates account positions after trades. """
+        new_positions = trades.groupby('symbol')['shares'].sum()
+        self.account_positions = self.account_positions.add(new_positions, fill_value=0)
 
     def __str__(self):
-            return '\n\n'.join(['{key}\n{value}'.format(key=key, value=self.__dict__.get(key)) for key in self.__dict__])
+        return '\n\n'.join([f'{key}\n{value}' for key, value in self.__dict__.items()])
--- trade_generator/trade_calculator.py
+++ trade_generator/trade_calculator.py
@@ -1,41 +1,60 @@
 import pandas as pd
 
-class TradeCalculator(object):
+class TradeCalculator:
     def __init__(self, portfolio, model, prices):
         """
         :param portfolio: portfolio object
         :param model: model_request attribute of trade_request object
         :param prices: prices attribute of prices object
         """
-        # print(prices)
-        # print('portfolio', '\n', portfolio.prices, '\n', portfolio.portfolio_positions)
         self.model = model
         self.portfolio = portfolio
         self.prices = prices
-        self.portfolio_trade_list = self._add_share_trades()
+        self.portfolio_trade_list = self._calculate_trades()
 
-    def _add_share_trades(self):
-        def share_trade(row):
-            if row.model_weight == 0:
-                return row.shares if row.shares <0 else row.shares * -1
-            else:
-                return (row.dollar_trades/row.price).round()
-        # todo: MUST DO THIS. Should not be rounding at this point. That should be done later.
-        trade_list = self._get_dollar_trades(self.portfolio, self.model.model_positions)
-        if trade_list is None: return None
-        trade_list['share_trades'] = trade_list.apply(share_trade, axis=1)
-        trade_list.drop(['shares'], axis=1, inplace=True)
+    def _calculate_trades(self):
+        trade_list = self._get_dollar_trades()
+        if trade_list is None:
+            return None
+
+        trade_list['share_trades'] = trade_list.apply(self._calculate_share_trade, axis=1)
+        trade_list.drop(columns=['shares'], inplace=True, errors='ignore')
         return trade_list
 
-    def _get_dollar_trades(self, portfolio, model_positions):
-        portfolio_model_weights = pd.concat([model_positions, portfolio.portfolio_positions['portfolio_weight'], portfolio.portfolio_positions['shares']], axis=1).fillna(0)
-        if portfolio_model_weights.empty: return None
-        portfolio_model_weights['price'] = self.prices
-        portfolio_model_weights['dollar_trades'] = ((portfolio_model_weights['model_weight'] - portfolio_model_weights['portfolio_weight']) * self.portfolio.portfolio_value).round(2)
+    def _calculate_share_trade(self, row):
+        """
+        Determines the number of shares to trade based on model weight.
+        """
+        if row.model_weight == 0:
+            return row.shares if row.shares < 0 else -row.shares
+        return round(row.dollar_trades / row.price)
 
-        portfolio_model_weights.index.name = 'symbol'
+    def _get_dollar_trades(self):
+        """
+        Computes dollar value trades by comparing portfolio allocation to model allocation.
+        """
+        if self.model.model_positions.empty:
+            return None
+
+        portfolio_model_weights = (
+            pd.concat(
+                [
+                    self.model.model_positions,
+                    self.portfolio.portfolio_positions[['portfolio_weight', 'shares']],
+                ],
+                axis=1
+            ).fillna(0)
+        )
+
+        portfolio_model_weights['price'] = self.prices
+        portfolio_model_weights['dollar_trades'] = (
+                (portfolio_model_weights['model_weight'] - portfolio_model_weights['portfolio_weight'])
+                * self.portfolio.portfolio_value
+        ).round(2)
+
         return portfolio_model_weights
 
-
     def __str__(self):
-            return '\n\n'.join(['{key}\n{value}'.format(key=key, value=self.__dict__.get(key)) for key in self.__dict__])
+        return '\n\n'.join(
+            [f'{key}\n{value}' for key, value in self.__dict__.items()]
+        )
