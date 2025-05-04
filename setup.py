c0cb03fd3434d9bde9d756d611542c6d2583acf9
ryelandgongora <rsgmon@gmail.com>
2025-05-04 11:16:16 -0700

    pfm_002: rename project
    Replace all references to portmgr with realloc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="realloc",
-    version="0.1.0",
+    version="0.1.1",
     description="A modular Python library for multi-account portfolio trade allocation",
     long_description="A modular Python library for multi-account portfolio trade allocation.",
     long_description_content_type="text/markdown",
