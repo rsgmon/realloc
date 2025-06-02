5564a738f2588f3d50ddcc6bd1e395a22816cd58
ryelandgongora <rsgmon@gmail.com>
2025-06-01 18:51:59 -0700

    realloc_05: Minimal Plugin
    Add a minimal plugin example.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="realloc",
-    version="0.1.1",
+    version="0.1.2",
     description="A modular Python library for multi-account portfolio trade allocation",
     long_description="A modular Python library for multi-account portfolio trade allocation.",
     long_description_content_type="text/markdown",
